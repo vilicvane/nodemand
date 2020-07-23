37eeb6b336ceb727112b1146fa61874622e31669
vilicvane <i@vilic.info>
2020-07-24 02:03:28 +0800

    Fallback to require.cache if ESMLoader not available

--- injection.js
+++ injection.js
@@ -6,8 +6,6 @@ const MODULE_PATH_FETCH_INTERVAL = 5000;
 
 const FILE_PROTOCOL_PREFIX = 'file:///';
 
-const {ESMLoader} = require('internal/process/esm_loader');
-
 let reportedModulePathSet = new Set();
 
 setTimeout(reportModulePaths, INITIAL_MODULE_PATH_FETCH_TIMEOUT).unref();
@@ -17,25 +15,9 @@ setInterval(reportModulePaths, MODULE_PATH_FETCH_INTERVAL).unref();
 process.on('exit', reportModulePaths);
 
 function reportModulePaths() {
-  let paths = Array.from(ESMLoader.moduleMap.keys())
-    .map(uri => {
-      if (!uri.startsWith(FILE_PROTOCOL_PREFIX)) {
-        return undefined;
-      }
-
-      let path = Path.normalize(
-        decodeURI(uri.slice(FILE_PROTOCOL_PREFIX.length)),
-      );
-
-      if (!Path.isAbsolute(path)) {
-        path = `${Path.sep}${path}`;
-      }
-
-      return path;
-    })
-    .filter(
-      path => typeof path === 'string' && !reportedModulePathSet.has(path),
-    );
+  let paths = fetchModulePaths().filter(
+    path => !reportedModulePathSet.has(path),
+  );
 
   for (let path of paths) {
     reportedModulePathSet.add(path);
@@ -45,4 +27,30 @@ function reportModulePaths() {
     type: 'add-paths',
     paths,
   });
+}
+
+function fetchModulePaths() {
+  try {
+    const {ESMLoader} = require('internal/process/esm_loader');
+
+    return Array.from(ESMLoader.moduleMap.keys())
+      .map(uri => {
+        if (!uri.startsWith(FILE_PROTOCOL_PREFIX)) {
+          return undefined;
+        }
+
+        let path = Path.normalize(
+          decodeURI(uri.slice(FILE_PROTOCOL_PREFIX.length)),
+        );
+
+        if (!Path.isAbsolute(path)) {
+          path = `${Path.sep}${path}`;
+        }
+
+        return path;
+      })
+      .filter(path => typeof path === 'string');
+  } catch (error) {
+    return Object.keys(require.cache);
+  }
 }
